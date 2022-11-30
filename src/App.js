465f876fe6ed39d433c3d0c6e0cb94b5029712f5
sebneedles <sebneedles@gmail.com>
2022-11-30 17:17:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCircleCheck,
+  faPen,
+  faTrashCan,
+} from '@fortawesome/free-solid-svg-icons';
+
 import './App.css';
 
 function App() {
+  // Tasks (ToDoLIst) State
+  const [toDo, setToDo] = useState([
+    { id: 1, title: 'Task 1', status: false },
+    { id: 2, title: 'Task 2', status: false },
+  ]);
+
+  // Temp State
+  const [newTask, setNewTask] = useState('');
+  const [updateData, setupdateData] = useState('');
+
+  // Add Task
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask('');
+    }
+  };
+
+  // Delete Task
+  const deleteTask = () => {};
+
+  // Mark Task as done or completed
+  const markDone = (id) => {};
+
+  // Cancel Update
+  const cancelUpdate = () => {};
+
+  // Change Task for update
+  const changeTask = (e) => {};
+
+  // Update Task
+  const updateTask = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      <br />
+      <br />
+      <h2>To Do List App (ReactJS)</h2>
+      <br />
+      <br />
+      {/* Update Task */}
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-bg" />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success mr-20">Update</button>
+          <button className="btn btn-lg btn-warning">Cancel</button>
+        </div>
+      </div>
+      <br />
+
+      {/* Add Task */}
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-bg"
+          />
+        </div>
+        <div className="col-auto">
+          <button onClick={addTask} className="btn btn-lg btn-success">
+            Add Task
+          </button>
+        </div>
+      </div>
+
+      {/* Display ToDos */}
+
+      {toDo && toDo.length ? '' : 'No Tasks...'}
+
+      {toDo &&
+        toDo
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? 'done' : ''}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="iconsWrap">
+                    <span title="Completed / Not Completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
     </div>
   );
 }
